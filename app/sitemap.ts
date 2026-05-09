b42cae03c6df66ffdb84880ea0fcf4a03a12e8af
adith004k <adith2004k@gmail.com>
2026-05-09 19:33:27 +0530

    add improved sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,25 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: "https://morocco-nature.vercel.app",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://morocco-nature.vercel.app/services",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://morocco-nature.vercel.app/bridal",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://morocco-nature.vercel.app/gallery",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://morocco-nature.vercel.app/contact",
             lastModified: new Date(),
         },
     ];
